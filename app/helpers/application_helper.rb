034db1ddeca091925b64f737589d1d55b9bcde16
Jordan Hudgens <jordan@devcamp.com>
2017-07-19 15:05:30 +0000

    updated refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+  GalpinViewTool::Renderer.copyright 'Maxwell Galpin', 'All rights reserved'
   end
 
   def nav_items
