2669670779251ff4b3445d80f456954e004fb3e9
Jordan Hudgens <jordan@devcamp.com>
2017-07-31 15:31:25 +0000

    added alerts for errors and alerts,

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def copyright_generator
-  GalpinViewTool::Renderer.copyright 'Maxwell Galpin', 'All rights reserved'
+      GalpinViewTool::Renderer.copyright 'Maxwell Galpin', 'All rights reserved'
   end
 
   def nav_items
@@ -59,4 +59,26 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Maxwell G", sticky: false)
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
